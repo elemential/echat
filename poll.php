69b821750e07073e2895b7f0721392fad9e1dcc7
Ben Snow <balintbence97@gmail.com>
2015-02-10 01:03:30 +0100

    Added polling and reformatted queries with sprintf  - verify.php might be an exception, don't forget to fix that too

---
+++ poll.php
@@ -0,0 +1,71 @@
+<?
+  // Cryptoblog Connection Tracker
+  // Copyleft by Elemential 2015
+  // Licensed under LGPL 3.0
+  
+  require_once("config.php");
+  require_once("verify.php");
+  require_once("message.php");
+  
+  $return_value = [ "valid" => false ];
+  
+  $con = CryptoblogConfig::getConnection();
+  
+  $inputMessage = new CryptoblogMessage($_REQUEST['data'], $_REQUEST['peerid'], CryptoblogMessage::ENCRYPTED);
+  
+  if( $inputMessage -> getValid() )
+  {
+    $data = $inputMessage -> getMessage();
+    
+    sleep(1);
+    
+    $query = sprintf(
+      "UPDATE %1\$s
+      SET last = CURRENT_TIMESTAMP
+      WHERE id = %2\$d",
+      CryptoblogConfig::getTableName("peers"),
+      intval($_REQUEST['peerid']));
+    $result = $con -> query($query);
+    
+    $return_data = [];
+    
+    $query = sprintf(
+      "SELECT id,pubkey
+      FROM %1\$s
+      WHERE room = (
+        SELECT room
+        FROM %1\$s
+        WHERE id = %2\$d
+        LIMIT 1
+      )
+      AND id != %2\$d
+      AND UNIX_TIMESTAMP(last)>%3\$d",
+      CryptoblogConfig::getTableName("peers"),
+      $_REQUEST['peerid'],
+      time()-10);
+    $result = $con -> query($query);
+    
+    if($con -> errno)
+    {
+      die($con -> error);
+    }
+    
+    $peers = [];
+    while($row = $result -> fetch_assoc())
+    {
+      $peers[] = [
+        "id" => $row["id"],
+        "key" => $row["pubkey"]
+      ];
+    }
+    
+    $return_data["peers"] = $peers;
+    
+    $message = new CryptoblogMessage($return_data, $_REQUEST['peerid'], CryptoblogMessage::DECRYPTED);
+    
+    $return_value["data"] = $message -> getMessage();
+    $return_value["valid"] = $message -> getValid();
+  }
+  
+  echo json_encode( $return_value );
+?>
